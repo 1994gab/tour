3d4bff931184bee40ed86c70680da939045288ab
1994gab <54586248+1994gab@users.noreply.github.com>
2022-02-13 13:44:41 +0200

    Add files via upload

---
+++ build/precache-manifest.8665be3f7d3af9d5e58a9308bd7d5354.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b191bbc19b6d3b797fe560542d1ed9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a644d42227b4d2108f52",
+    "url": "/static/css/main.0a2fde1e.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a644d42227b4d2108f52",
+    "url": "/static/js/main.c371da34.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
